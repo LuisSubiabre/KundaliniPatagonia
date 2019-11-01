6c112bc8c20c801e88bd5a21b8cdd627605d3ae9
luissubiabre <luissubiabre@hotmail.com>
2019-10-31 22:07:17 -0300

    mailer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const modemailer = require('nodemailer');
 const app = express();
 
 // settings
